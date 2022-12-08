e99f25efb9a88bcae6b74d64a18d229bba1ab9ee
Amaan Q <amaanq12@gmail.com>
2022-12-07 20:22:12 -0500

    zzz

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,12 @@
 extern crate proc_macro;
 
 use proc_macro::TokenStream;
-use quote::quote;
 use syn::{parse_macro_input, DeriveInput};
 
-mod message;
+mod bytestream;
 
-/// Generate `bytestream_io::ByteStreamIo` implementation for structs and enums that have all fields implement `ByteStreamIo`.
+/// Generate `ByteStream` implementation for structs and enums that have all fields
+/// implement `ByteStream`.
 ///
 /// For structs, fields are written one by one in order.
 ///
@@ -14,10 +14,10 @@ mod message;
 /// the enum, followed by the fields of the enum one by one. If the enum repr should be little
 /// endian, the `#[little_endian]` attribute must be applied on the `enum` item. If the enum repr
 /// should be encoded as a `VInt`, the `#[VInt]` attribute must be applied on the `enum` item.
-#[proc_macro_derive(Packet, attributes(little_endian, VInt))]
+#[proc_macro_derive(ByteStream, attributes(little_endian, VInt))]
 pub fn derive_message(item: TokenStream) -> TokenStream {
     let parsed = parse_macro_input!(item as DeriveInput);
-    match message::imp(&parsed) {
+    match bytestream::imp(&parsed) {
         Ok(item) => item.into(),
         Err(err) => err.to_compile_error().into(),
     }
